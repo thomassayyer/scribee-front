0585ad39f66991fa0f81fea0828e25c02d110893
Thomas Sayer <thomas.sayyer@gmail.com>
2019-07-16 20:39:20 +0200

    NEW: store

---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+  state: {
+    quotes: [
+      { author: "Isocrate", quote: "Nous voyons l'abeille se poser sur toutes les plantes et tirer de chacune le meilleur." },
+      { author: "Montesquieu", quote: "Ce qui n'est point utile à l'essaim, n'est point utile à l'abeille." },
+    ]
+  }
+})
